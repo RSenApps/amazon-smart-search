3c2c1e98586f4b442206e930687d9145dfe025e0
Ryan Senanayake <rsen@mit.edu>
2021-04-22 22:17:32 -0400

    Parallel fetches from review meta

--- fetcher.py
+++ fetcher.py
@@ -4,6 +4,7 @@ from mezmorize import Cache
 import pandas as pd
 import json
 import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
 cache = Cache(DEBUG=True, CACHE_TYPE='filesystem', CACHE_DIR='cache', CACHE_THRESHOLD=1024*1024, CACHE_DEFAULT_TIMEOUT=9999)
 
 
@@ -36,7 +37,12 @@ def isfloat(value):
 
 
 def adjust_reviews(products_df):
-    review_meta_dict = {asin : fetch_review_meta(asin) for asin in products_df["asin"]}
+    processes = []
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for asin in products_df["asin"]:
+            processes.append(executor.submit(fetch_review_meta, asin))
+
+    review_meta_dict = {x.result()[0] : x.result()[1] for x in as_completed(processes)}
     has_reviews = [k for k,v in review_meta_dict.items() if isfloat(v['rating'])]
     products_df.loc[products_df["asin"].isin(has_reviews), "reviews.rating"] = products_df["asin"].map({k : v["rating"] for k, v in review_meta_dict.items()})
     products_df['reviews.rating'] = products_df['reviews.rating'].astype(float)
@@ -46,4 +52,4 @@ def adjust_reviews(products_df):
 @cache.memoize()
 def fetch_review_meta(asin):
     print("Fetching: ", asin)
-    return requests.get("https://reviewmeta.com/api/amazon/" + asin).json()
+    return (asin, requests.get("https://reviewmeta.com/api/amazon/" + asin).json())
